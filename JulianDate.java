1b3c051f805692b6cc25b67741fdfdacb2a6f527
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-05-27 20:53:59 +0200

    Used a ready formula, weekdays are not correct

--- JulianDate.java
+++ JulianDate.java
@@ -1,8 +1,8 @@
 import java.time.LocalDateTime;
 
 /**
- * This class takes any date and converts it to a Julian Date
- * otherwise takes a Julian Date and converts it back to Gregorian.
+ * This class takes any date and converts it to a Julian Date otherwise takes a
+ * Julian Date and converts it back to Gregorian.
  * 
  * @author Aidaika09
  * @author n-c0de-r
@@ -11,35 +11,33 @@ import java.time.LocalDateTime;
  */
 
 public class JulianDate {
+
 	private static final LocalDateTime CURRENT_DATE = LocalDateTime.of(-4712, 1, 1, 12, 0, 0, 0);
-	
-	private double julianDate;
+
+	private int julianDate;
+	private int julianTime;
 	private LocalDateTime gregorianDate;
-	
+
 	/**
-	 * Overload the class with several constructors to guard against
-	 * passing to few arguments 
+	 * Overload the class with several constructors to guard against passing to few
+	 * arguments
 	 */
-	public JulianDate (int year) {
-		this(year, 1, 1, 0, 0, 0);
+	public JulianDate(int number) {
+		//converts back to gregorian.
 	}
-	
-	public JulianDate (int year, int month) {
-		this(year, month, 1, 0, 0, 0);
-	}
-	
-	public JulianDate (int year, int month, int day) {
+
+	public JulianDate(int year, int month, int day) {
 		this(year, month, day, 0, 0, 0);
 	}
-	
-	public JulianDate (int year, int month, int day, int hrs) {
+
+	public JulianDate(int year, int month, int day, int hrs) {
 		this(year, month, day, hrs, 0, 0);
 	}
-	
-	public JulianDate (int year, int month, int day, int hrs, int min) {
+
+	public JulianDate(int year, int month, int day, int hrs, int min) {
 		this(year, month, day, hrs, min, 0);
 	}
-	
+
 	/**
 	 * Main constructor with all parameters to create a Julian Date
 	 * 
@@ -50,47 +48,47 @@ public class JulianDate {
 	 * @param min
 	 * @param sec
 	 */
-	public JulianDate (int year, int month, int day, int hrs, int min, int sec) {
-		int february = 0;
+	public JulianDate(int year, int month, int day, int hrs, int min, int sec) {
+		//Pretty much a copy of www.youtube.com/watch?v=A9KJQje-340&t=488s
+		//It's much simpler and cleaner to read than the formula of Wikipedia!
 		
-		// Add year number
-		julianDate = (year + 4712) * 1000000000;
+		int a = (14-month) / 12;	//calculate the average days in a month?
+		int y = year + 4800 - a;	// calculate the average of years?
+		int m = month + 12 * a - 3; //calculate the average of months in a year?
 		
-		//Check if it's a leap year, and set the smallest month length
-		if (year % 4 == 0) {
-			february = 29;
-		} else {
-			february = 28;
-		}
+		julianDate = day + (153 * m + 2) / 5 + (365 * y) + y/4 + y/100 + y/400  - 32045;
 		
-		//If the current day is smaller than the minimum month length
-		//don't include the current month in multiplication.
-		if (day < february) {
-			month += (month-1);
-		}
-		
-		// Add day numbers
-		julianDate += (month * february) + (day % february) * 1000000;
-		
-		//Add hours
-		julianDate += (hrs % 24) * 10000;
-		
-		//Add hours
-		julianDate += (min % 60) * 100;
-		
-		//Add hours
-		julianDate += sec;
+		julianTime = julianDate + hrs/24 + min/1440 + sec/86400;
 	}
-	
-	public double getJulian_() {
+
+	public int getDays() {
 		return julianDate;
 	}
-	
-	public LocalDateTime getGregorianDate() {
-		return gregorianDate;
+
+	public int getTime() {
+		return julianTime;
 	}
-	
-	public void getJulianDate() {
-		System.out.println();
+
+	public String getWeekday() {
+		int w = (julianTime % 7);
+		switch (w) {
+		case 1:
+			return "Monday";
+		case 2:
+			return "Tuesday";
+		case 3:
+			return "Wednesday";
+		case 4:
+			return "Thursday";
+		case 5:
+			return "Friday";
+		case 6:
+			return "Saturday";
+		case 0:
+			return "Sunday";
+		default:
+			return "not a weekday";
+		}
 	}
+
 }

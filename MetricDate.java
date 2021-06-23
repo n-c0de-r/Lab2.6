46a4cadec965531d93ba39ab712e505f152928b8
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-06-23 22:44:24 +0200

    Update MetricDate.java

--- MetricDate.java
+++ MetricDate.java
@@ -1,4 +1,9 @@
-
+/**
+ * This class takes JulianDate types and converts them to MetricDates
+ * 
+ * @author HTW-IMI
+ * @version 2021.06.23
+ */
 public class MetricDate {
 
 	private int days;
@@ -6,10 +11,30 @@ public class MetricDate {
 	private int months;
 	private int years;
 	
+	/**
+	 * Empty constructor
+	 */
 	public MetricDate() {
-		
 	}
 	
+	/**
+	 * Constructor to convert regular dates to Metric
+	 *
+	 * @param year
+	 * @param month
+	 * @param day
+	 */
+	public MetricDate(int year, int month, int day) {
+		//Only considers the full julian date, needs subtraction of current dates!
+		JulianDate jd = new JulianDate(year, month, day);
+		toMetricDate(jd);
+	}
+	
+	/**
+	 * Method to convert JulianDates to MetricDates.
+	 *
+	 * @param date	Object of type JulianDate.
+	 */
 	public void toMetricDate(JulianDate date) {
 		int num = date.getNumber();
 		days = num % 10;
@@ -18,6 +43,11 @@ public class MetricDate {
 		years = num / 1000;
 	}
 	
+	/**
+	 * Convert any MetricDate to JulianDate number
+	 *
+	 * @return	Recalculated JulianDate number
+	 */
 	public int toJulianDate() {
 		int num = 0;
 		num += years * 1000;
@@ -27,6 +57,11 @@ public class MetricDate {
 		return num;
 	}
 	
+	/**
+	 * Accessor method to get the MetricDate value.
+	 *
+	 * @return	A String representation of the MetricDate.
+	 */
 	public String getValue() {
 		return "" + years + "." + months + "." + weeks + "." + days;
 	}
